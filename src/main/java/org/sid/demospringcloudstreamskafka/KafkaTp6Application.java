21908672deb4817ccd6936f8f61dc18147043f8c
Ennia-Fahd <enniafahd@gmail.com>
2024-01-07 21:34:10 +0100

    first commit

---
+++ src/main/java/org/sid/demospringcloudstreamskafka/KafkaTp6Application.java
@@ -0,0 +1,13 @@
+package org.sid.demospringcloudstreamskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTp6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTp6Application.class, args);
+    }
+
+}
